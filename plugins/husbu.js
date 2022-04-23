59626f00889a9628118f9bced728926abf228fc2
APTXmrnn <45924038+APTXmrnn@users.noreply.github.com>
2022-04-23 10:02:35 +0700

    Update husbu.js
    api melchanz

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,5 +1,10 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/husbu', {}, 'apikey'), 'Nih', wm, 'NEXT', 'husbu', m)
+  let res = await fetch('https://melcanz.com/husbu?&apikey=NOVENSxd')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'nyoh husbu mu', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['husbu']
 handler.tags = ['anime']
