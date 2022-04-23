36a99bdf20199d40d94356375ddab05892b90950
APTXmrnn <45924038+APTXmrnn@users.noreply.github.com>
2022-04-23 21:41:46 +0700

    Create cecanmalay.js

---
+++ plugins/cecanmalay.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let res = await fetch(API('amel', '/malay', {}, 'apikey'))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (!json.status) throw json
+    conn.sendButtonImg(m.chat, json.result, 'Nieh banh', wm, 'lagi', `${usedPrefix + command}`, m)
+}
+handler.help = ['cecanmalay']
+handler.tags = ['cecan']
+handler.command = /^(cecanmalay)$/i
+
+handler.limit = true
+module.exports = handler
